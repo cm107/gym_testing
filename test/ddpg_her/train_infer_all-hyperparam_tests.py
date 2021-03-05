d5d92fe31ce21d71a51c98b9872771a44049116b
Clayton Mork <mork.clayton3@gmail.com>
2021-03-05 17:31:47 +0900

    DDPG_HER hyperparameter test tweak

--- test/ddpg_her/train_infer_all-hyperparam_tests.py
+++ test/ddpg_her/train_infer_all-hyperparam_tests.py
@@ -15,15 +15,13 @@ env_name_list = [
 output_root_dir = 'robotics_output'
 make_dir_if_not_exists(output_root_dir)
 
-for max_epochs in [500]:
-    for penalize_actions in [True, False]:
-        target_dirname = f'epoch{max_epochs}'
-        if not penalize_actions:
-            target_dirname += "-no_action_penalty"
-        target_output_dir = f'{output_root_dir}/{target_dirname}'
-        make_dir_if_not_exists(target_output_dir)
+for env_name in env_name_list:
+    for max_epochs in [2000]:
+        for lr, lr_name in [(1e-3, '1e-3'), (5e-4, '5e-4'), (1e-4, '1e-4')]:
+            target_dirname = f'epoch{max_epochs}_lr{lr_name}'
+            target_output_dir = f'{output_root_dir}/{target_dirname}'
+            make_dir_if_not_exists(target_output_dir)
 
-        for env_name in env_name_list:
             env_root_dir = f'{target_output_dir}/{env_name}'
             make_dir_if_not_exists(env_root_dir)
             delete_all_files_in_dir(env_root_dir, ask_permission=False)
@@ -35,13 +33,14 @@ for max_epochs in [500]:
             print(f'========{env_name}=========')
             trainer = DDPG_HER_Trainer(
                 weight_path=weight_path,
-                env_name=env_name
+                env_name=env_name,
+                actor_lr=lr, critic_lr=lr
             )
             trainer.train(
                 max_epochs=max_epochs, max_cycles=50,
                 max_episodes=2, num_updates=40,
                 n_timesteps=50, nontrivial_goal_thresh=0.05,
-                penalize_actions=penalize_actions,
+                penalize_actions=True,
                 plot_save_path=plot_path,
                 plot_include=['success', 'actor', 'critic'],
                 log_dir=log_dir
